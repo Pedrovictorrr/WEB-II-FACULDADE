6d52a2f896f85d07c46a9f911ce12e651b40b264
Pedrovictorrr <82172897+Pedrovictorrr@users.noreply.github.com>
2022-01-25 10:31:02 -0300

    Update and rename README..js to EXER1.js

---
+++ EXER1.js
@@ -0,0 +1,15 @@
+      //Questao 1
+document.write("Questao 1")
+document.write(" 1) Boa vindas " + prompt('Digite teu nome') + "<br>")
+var n1 = prompt('Digite um numero')
+var n2 = prompt('Digite outro numero')
+var n3 = n1 + n2
+var n4 = n1 * n2
+var n5 = n1 / n2
+var n6 = n1 - n2
+      //Questao 2
+document.write("Questao 2")
+document.write ("a) a soma destes valores é: " + n3 + "<br>")
+document.write ("b) O produto deles é: " + n4 + "<br>")
+document.write ("c) O quociente entre eles é: " + n5 + "<br>")
+document.write ("d) A diferença entre eles é: " + n6 + "<br>")
