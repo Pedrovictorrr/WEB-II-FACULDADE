18423d50397f5a713b267dc0c4182da51116fd72
Pedrovictorrr <82172897+Pedrovictorrr@users.noreply.github.com>
2022-01-25 10:30:08 -0300

    Update and rename README.md to README..js

---
+++ README..js
@@ -0,0 +1,35 @@
+# WEB-II-FACULDADE
+Exercicios e Provas
+Passos a serem desenvolvidos no Lab 1:
+
+1. Desenvolver a lista abaixo, utilizando JS.
+2. Todas as questões da lista, devem ser desenvolvidas em um único arquivo html e js
+3. Entregar a lista na data de entrega 18/08, sem atraso.
+4. Não esquecer de apertar o botão de entregar.
+
+1.Crie um script JS que leia o nome de uma pessoa e mostre uma mensagem de Boas Vindas de acordo com o nome digitado.
+
+2.Ler dois valores a partir do prompt, calcular e imprimir na tela:
+a) A soma destes valores;
+b)O produto deles;
+c)O quociente entre eles;
+d)A diferença entre eles.
+
+3.Elabore um programa em JS que calcule a média aritmética de duas notas e exiba o resultado na tela.
+
+
+      //Questao 1
+document.write("Questao 1")
+document.write(" 1) Boa vindas " + prompt('Digite teu nome') + "<br>")
+var n1 = prompt('Digite um numero')
+var n2 = prompt('Digite outro numero')
+var n3 = n1 + n2
+var n4 = n1 * n2
+var n5 = n1 / n2
+var n6 = n1 - n2
+      //Questao 2
+document.write("Questao 2")
+document.write ("a) a soma destes valores é: " + n3 + "<br>")
+document.write ("b) O produto deles é: " + n4 + "<br>")
+document.write ("c) O quociente entre eles é: " + n5 + "<br>")
+document.write ("d) A diferença entre eles é: " + n6 + "<br>")
--- README.md
+++
@@ -1,2 +0,0 @@
-# WEB-II-FACULDADE
-Exercicios e Provas
